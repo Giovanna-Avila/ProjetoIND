ba1008bb4b66c2a44561296f368783166dc7d1aa
Giovanna-Avila <giovannaarodrigues@sptech.school>
2023-05-30 21:43:06 -0300

    atualização site

---
+++ site oficial/site/src/controllers/userController.js
@@ -0,0 +1,58 @@
+var empresaModel = require("../models/userModel");
+
+function listar(req, res) {
+    userModel.listar().then(function (resultado) {
+        // precisamos informar que o resultado voltará para o front-end como uma resposta em json
+        res.status(200).json(resultado);
+    }).catch(function (erro) {
+        res.status(500).json(erro.sqlMessage);
+    })
+}
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeServer;
+    var sobrenome = req.body.sobrenomeServer;
+    var dtNasc = req.body.dtNascServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    
+
+   
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (sobrenome == undefined) {
+        res.status(400).send("Seu sobrenome está undefined!");
+    } else if (dtNasc == undefined) {
+        res.status(400).send("Sua data de nascimento está undefined!");
+    }  else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        userModel.cadastrar(nome, sobrenome, dtNasc,email, senha)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    cadastrar,
+    listar,
+ 
+}
